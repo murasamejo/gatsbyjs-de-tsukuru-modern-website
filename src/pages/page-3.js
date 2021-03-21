572ca0785f3c50b0889da93e9338c6d9264bd499
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-03-21 17:03:18 +0900

    feat: 🎸 page-3 に styled-components を適用した

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -1,12 +1,21 @@
 import React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 
+const RedString = styled.div`
+  color: red;
+  font-size: 20px;
+`
+
 const IndexPage = () => (
-  <Layout>
-    <h1>My First Component</h1>
-    <Link to="/">Back to Home</Link>
-  </Layout>
+  <RedString>
+    <Layout>
+      <h1>My First Component</h1>
+      <Link to="/">Back to Home</Link>
+    </Layout>
+  </RedString>
 )
+
 export default IndexPage
