b8b07286f48e10c6763778437658523ce5c34b94
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-02-07 18:47:43 +0900

    feat: 🎸 詳細ページ作成

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-const Post = () => {
-  return <p>This is the post template.</p>
+import Layout from '../components/layout'
+
+const Post = props => {
+  const post = props.data.contentfulPost
+  const author = post.author
+  const avatar = author.avatar
+
+  // published_at カラムは publishedAt になることに注意する
+  return (
+    <Layout>
+      <h1>{post.title}</h1>
+      {avatar && (
+        <img width={40} src={avatar} alt={author.name} />
+      )}
+      <small>
+        {author.name} | {post.publishedAt}
+      </small>
+      <div>post.content</div>
+    </Layout>
+  )
 }
 
 export default Post
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulPost(slug: { eq: $slug }) {
+      title
+      content {
+        content
+      }
+      author {
+        name
+        description {
+          description
+        }
+        avatar
+      }
+      publishedAt(formatString: "YYYY-MM-DD")
+    }
+  }
+`
