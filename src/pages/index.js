bc162a1b182e1aa07e83994a4940b6de8e2c4f5d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-02-07 16:20:27 +0900

    feat: 🎸 Contentful 導入前にいったん戻す

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const IndexPage = () => (
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/page-3/">Go to page 3</Link> <br />
-      <Link to="/page-4/">Go to page 4</Link> <br />
+      {/* <Link to="/page-4/">Go to page 4</Link> <br /> */}
       <Link to="/data/">Go to data page</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
